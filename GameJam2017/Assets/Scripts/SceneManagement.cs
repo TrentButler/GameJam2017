3e1c6360238a38777c9025400a5b85b53d2020b2
dylanmitchell1007 <armadummy@yahoo.com>
2017-10-25 17:36:43 -0500

    MainMenu Scene/Garage Scene

---
+++ GameJam2017/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+
+{
+
+    public void MainMenuLoad()
+    {
+        SceneManager.LoadScene("0.main", LoadSceneMode.Single);
+    }
+    public void LoadGarage()
+    {
+        SceneManager.LoadScene("1.garage", LoadSceneMode.Single);
+    }
+    // Use this for initialization
+
+ 
+    // Update is called once per frame
+
+}
