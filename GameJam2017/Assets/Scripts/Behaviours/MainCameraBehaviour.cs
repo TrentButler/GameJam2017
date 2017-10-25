8bcb4a82d6fa729719f9f0cd25eb9197080381ca
Trent Butler <s178061@students.aie.edu.au>
2017-10-25 17:35:57 -0500

    vehicle movement, main camera behaviour

---
+++ GameJam2017/Assets/Scripts/Behaviours/MainCameraBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraBehaviour : MonoBehaviour {
+
+    
+    private Transform car;
+    private Transform target;
+    private Transform Camera;
+
+    public float CamX = 0;
+    public float CamY = 10;
+    public float CamZ = -35;
+    public float CameraTurnAngle = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        car = GameObject.FindGameObjectWithTag("chassis").transform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //CHECK THE PLAYER STATE
+        //DETERMINE THE CAMERA'S 'target'
+
+        target = car;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            CamZ += 1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            CamZ -= 1.0f;
+        }
+
+        //var angle = Input.GetAxis("Steer");
+        var angle = 1.0f;
+
+        //Vector3 position = new Vector3(CamX += angle, CamY, CamZ);
+        Vector3 position = new Vector3(CamX, CamY, CamZ);
+
+        Camera.transform.LookAt(target);
+        //Camera.Rotate(Vector3.up, angle * CameraTurnAngle);        
+        Camera.position = target.position + position;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+}
---
+++ GameJam2017/Assets/Scripts/Behaviours/VehicleMovementBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovementBehaviour : MonoBehaviour
+{
+
+    public float HorizontalWheelOffset = 2.0f;
+    public float AxelOffset = 2.0f;
+
+    public float MovementSpeed = 10.0f;
+    public float MaxSteerAngle = 45.0f;
+
+    private Transform chassis;
+
+    #region Wheels
+    Transform FR;
+    Transform FL;
+
+    Transform RR;
+    Transform RL;
+
+    WheelCollider wc_FL;
+    WheelCollider wc_FR;
+
+    WheelCollider wc_RL;
+    WheelCollider wc_RR;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        chassis = GameObject.FindGameObjectWithTag("chassis").GetComponent<Transform>();
+
+        #region GetWheels
+        FR = GameObject.FindGameObjectWithTag("FL").GetComponent<Transform>();
+        FL = GameObject.FindGameObjectWithTag("FR").GetComponent<Transform>();
+        RR = GameObject.FindGameObjectWithTag("RL").GetComponent<Transform>();
+        RL = GameObject.FindGameObjectWithTag("RR").GetComponent<Transform>();
+
+        wc_FL = GameObject.FindGameObjectWithTag("wcFL").GetComponent<WheelCollider>();
+        wc_FR = GameObject.FindGameObjectWithTag("wcFR").GetComponent<WheelCollider>();
+        wc_RL = GameObject.FindGameObjectWithTag("wcRL").GetComponent<WheelCollider>();
+        wc_RR = GameObject.FindGameObjectWithTag("wcRR").GetComponent<WheelCollider>();
+        #endregion
+
+        //OFFSET THE RIGHT WHEEL FROM THE LEFT
+        //OFFSET THE REAR WHEELS FROM THE FRONT
+        FR.position = new Vector3(FL.position.x + HorizontalWheelOffset, FL.position.y, FL.position.z);
+
+        RL.position = new Vector3(FL.position.x, FL.position.y, FL.position.z - AxelOffset);
+        RR.position = new Vector3(FL.position.x + HorizontalWheelOffset, FL.position.y, FL.position.z - AxelOffset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        //ACCELERATE
+        //BRAKE
+        //STEER
+
+        //OUTRUN DRIFTING
+        //CALCULATE VEHICLE SPEED (FOR UI)
+        //CALCULATE WHEEL RPM (FOR UI)
+        //CLEAN THIS UP
+
+        //ROTATE THE CHASSIS BASED ON THE WHEEL ANGLE
+        
+        var accel = Input.GetAxis("Accelerate");
+        var brake = Input.GetAxis("Brake");
+        var steer = Input.GetAxis("Steer");
+
+        Vector3 move = new Vector3(0, 0, accel * MovementSpeed);
+
+        //ROTATE THE WHEEL COLLIDERS
+        wc_FL.steerAngle = steer * MaxSteerAngle;
+        wc_FR.steerAngle = steer * MaxSteerAngle;
+
+        wc_RL.motorTorque = accel * MovementSpeed;
+        wc_RR.motorTorque = accel * MovementSpeed;
+
+        chassis.position += move * Time.deltaTime;
+    }
+}
