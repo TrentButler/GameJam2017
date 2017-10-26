18ee4e63cae0ed89ccbaf07ca4d88fa599b4c0e2
Trent Butler <s178061@students.aie.edu.au>
2017-10-26 14:25:19 -0500

    broken vehicle

---
+++ GameJam2017/Assets/Scripts/Behaviours/TestVehicleMovementBehaviour.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVehicleMovementBehaviour : MonoBehaviour
+{
+    public enum Gear
+    {
+        NEUTRAL = 0,
+        FIRST = 1,
+        SECOND = 2,
+        THIRD = 3,
+        FOURTH = 4,
+        FIFTH = 5,
+        REVERSE = 6,
+    }
+
+    private Gear transmission = Gear.NEUTRAL;
+
+    public float MaxVehicleSpeed = 10.0f;
+    public float MovementSpeed = 10.0f;
+    public float MaxSteerAngle = 45.0f;
+
+    //public float first = 0.0f;
+    //public float second = 0.0f;
+    //public float third = 0.0f;
+    //public float fourth = 0.0f;
+    //public float fifth = 0.0f;
+
+    public Vector3 velocity = Vector3.zero;
+    private Vector3 force = Vector3.zero;
+
+    private float torque = 0.0f;
+    private float MaxWheelTorque = 10.0f;
+    private float MaxBrakeForce = 10000.0f;
+
+    private Transform chassis;
+    private Rigidbody rb;
+
+    #region Wheels
+    Transform FR;
+    Transform FL;
+    Transform RR;
+    Transform RL;
+
+    WheelCollider wc_FL;
+    WheelCollider wc_FR;
+    WheelCollider wc_RL;
+    WheelCollider wc_RR;
+    #endregion
+
+    //private void Transmission()
+    //{
+    //    //LIMIT SPEED BY THE GEAR
+    //    switch (transmission)
+    //    {
+    //        case (Gear.NEUTRAL):
+    //            {
+    //                MaxVehicleSpeed = 0.0f;
+    //                if (velocity.magnitude > MaxVehicleSpeed)
+    //                {
+    //                    //SLOW DOWN
+    //                }
+    //                break;
+    //            }
+
+    //        case (Gear.FIRST):
+    //            {
+    //                MaxVehicleSpeed = first;
+    //                if (velocity.magnitude > MaxVehicleSpeed)
+    //                {
+    //                    //SLOW DOWN
+    //                }
+    //                break;
+    //            }
+
+    //        case (Gear.SECOND):
+    //            {
+    //                MaxVehicleSpeed = second;
+    //                if (velocity.magnitude > MaxVehicleSpeed)
+    //                {
+    //                    //SLOW DOWN
+    //                }
+    //                break;
+    //            }
+
+    //        case (Gear.THIRD):
+    //            {
+    //                MaxVehicleSpeed = third;
+    //                if (velocity.magnitude > MaxVehicleSpeed)
+    //                {
+    //                    //SLOW DOWN
+    //                }
+    //                break;
+    //            }
+
+    //        case (Gear.FOURTH):
+    //            {
+    //                MaxVehicleSpeed = fourth;
+    //                if (velocity.magnitude > MaxVehicleSpeed)
+    //                {
+    //                    //SLOW DOWN
+    //                }
+    //                break;
+    //            }
+
+    //        case (Gear.FIFTH):
+    //            {
+    //                MaxVehicleSpeed = fifth;
+    //                if (velocity.magnitude > MaxVehicleSpeed)
+    //                {
+    //                    //SLOW DOWN
+    //                }
+    //                break;
+    //            }
+
+    //        case (Gear.REVERSE):
+    //            {
+    //                MaxVehicleSpeed = 0.0f;
+    //                break;
+    //            }
+    //    }
+    //}
+
+    void Start()
+    {
+        chassis = GameObject.FindGameObjectWithTag("chassis").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+
+        FR = GameObject.FindGameObjectWithTag("FL").GetComponent<Transform>();
+        FL = GameObject.FindGameObjectWithTag("FR").GetComponent<Transform>();
+        RR = GameObject.FindGameObjectWithTag("RL").GetComponent<Transform>();
+        RL = GameObject.FindGameObjectWithTag("RR").GetComponent<Transform>();
+
+        wc_FL = GameObject.FindGameObjectWithTag("wcFL").GetComponent<WheelCollider>();
+        wc_FR = GameObject.FindGameObjectWithTag("wcFR").GetComponent<WheelCollider>();
+        wc_RL = GameObject.FindGameObjectWithTag("wcRL").GetComponent<WheelCollider>();
+        wc_RR = GameObject.FindGameObjectWithTag("wcRR").GetComponent<WheelCollider>();
+    }
+
+    void Update()
+    {
+        Debug.Log(transmission);
+    }
+
+    private void FixedUpdate()
+    {
+        //OUTRUN DRIFTING
+        //CALCULATE VEHICLE SPEED (FOR UI)
+        //CALCULATE WHEEL RPM (FOR UI)
+        //CLEAN THIS UP
+
+        var accel = Input.GetAxis("Accelerate");
+        var brake = Input.GetAxis("Brake");
+        var steer = Input.GetAxis("Steer");
+
+        #region Drive
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (transmission != Gear.REVERSE)
+            {
+                transmission += 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (transmission != Gear.NEUTRAL)
+            {
+                transmission -= 1;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity = Vector3.zero;
+        }
+
+        //Transmission();
+
+        ////LIMIT THE MAX SPEED
+        //if (velocity.magnitude > MaxVehicleSpeed)
+        //{
+        //    torque = 0.0f;
+        //}
+
+        //if (velocity.magnitude < MaxVehicleSpeed)
+        //{
+        //    //ADD TORQUE
+        //    torque += accel * MaxWheelTorque;
+        //}
+
+        torque = accel * MaxVehicleSpeed;
+
+        force = new Vector3(0, 0, torque * MovementSpeed);
+        //force = new Vector3(0, 0, torque);
+
+        //Vector3 acceleration = force * Time.deltaTime;
+        //velocity = velocity + (acceleration * Time.deltaTime);
+        velocity = force;
+        #endregion
+
+        #region Steer
+        //LIMIT THIS BASED ON VEHICLE SPEED
+        chassis.Rotate(new Vector3(0, steer * 0.5f, 0));
+        chassis.Translate(velocity);
+        #endregion
+
+        #region Visuals
+        wc_FL.steerAngle = steer * MaxSteerAngle;
+        wc_FR.steerAngle = steer * MaxSteerAngle;
+
+        Vector3 flPosition = new Vector3();
+        Quaternion flRotation = new Quaternion();
+        wc_FL.GetWorldPose(out flPosition, out flRotation);
+
+        Vector3 frPosition = new Vector3();
+        Quaternion frRotation = new Quaternion();
+        wc_FR.GetWorldPose(out frPosition, out frRotation);
+
+        FL.rotation = flRotation;
+        FL.position = flPosition;
+
+        FR.rotation = frRotation;
+        FR.position = frPosition;
+        #endregion
+    }
+}
--- GameJam2017/Assets/Scripts/Behaviours/VehicleMovementBehaviour.cs
+++ GameJam2017/Assets/Scripts/Behaviours/VehicleMovementBehaviour.cs
@@ -16,19 +16,26 @@ public class VehicleMovementBehaviour : MonoBehaviour
     }
 
     private Gear transmission = Gear.NEUTRAL;
-    
+
+    public float MaxVehicleSpeed = 0.0f;
     public float MovementSpeed = 10.0f;
     public float MaxSteerAngle = 45.0f;
+    public float driftForce = 1.0f;
 
-    public float RRwheelRPM = 0.0f;
-    public float RLwheelRPM = 0.0f;
+    public float first = 0.0f;
+    public float second = 0.0f;
+    public float third = 0.0f;
+    public float fourth = 0.0f;
+    public float fifth = 0.0f;
+
+    public Vector3 velocity = Vector3.zero;
+    private Vector3 force = Vector3.zero;
 
     private float torque = 0.0f;
-    private float MaxWheelRPM = 0.0f;
-
     private float MaxWheelTorque = 10.0f;
     private float MaxBrakeForce = 10000.0f;
-    
+
+    private Transform chassis;
     private Rigidbody rb;
 
     #region Wheels
@@ -50,43 +57,67 @@ public class VehicleMovementBehaviour : MonoBehaviour
         {
             case (Gear.NEUTRAL):
                 {
-                    MaxWheelRPM = 0.0f;
+                    MaxVehicleSpeed = 0.0f;
+                    if (velocity.magnitude > MaxVehicleSpeed)
+                    {
+                        //SLOW DOWN
+                    }
                     break;
                 }
 
             case (Gear.FIRST):
                 {
-                    MaxWheelRPM = 2500.0f;
+                    MaxVehicleSpeed = first;
+                    if (velocity.magnitude > MaxVehicleSpeed)
+                    {
+                        //SLOW DOWN
+                    }
                     break;
                 }
 
             case (Gear.SECOND):
                 {
-                    MaxWheelRPM = 3500.0f;
+                    MaxVehicleSpeed = second;
+                    if (velocity.magnitude > MaxVehicleSpeed)
+                    {
+                        //SLOW DOWN
+                    }
                     break;
                 }
 
             case (Gear.THIRD):
                 {
-                    MaxWheelRPM = 4500.0f;
+                    MaxVehicleSpeed = third;
+                    if (velocity.magnitude > MaxVehicleSpeed)
+                    {
+                        //SLOW DOWN
+                    }
                     break;
                 }
 
             case (Gear.FOURTH):
                 {
-                    MaxWheelRPM = 5500.0f;
+                    MaxVehicleSpeed = fourth;
+                    if (velocity.magnitude > MaxVehicleSpeed)
+                    {
+                        //SLOW DOWN
+                    }
                     break;
                 }
 
             case (Gear.FIFTH):
                 {
-                    MaxWheelRPM = 6500.0f;
+                    MaxVehicleSpeed = fifth;
+                    if (velocity.magnitude > MaxVehicleSpeed)
+                    {
+                        //SLOW DOWN
+                    }
                     break;
                 }
 
             case (Gear.REVERSE):
                 {
-                    MaxWheelRPM = 0.0f;
+                    MaxVehicleSpeed = 0.0f;
                     break;
                 }
         }
@@ -94,19 +125,20 @@ public class VehicleMovementBehaviour : MonoBehaviour
 
     void Start()
     {
+        chassis = GameObject.FindGameObjectWithTag("chassis").GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
-        
-        //FR = GameObject.FindGameObjectWithTag("FL").GetComponent<Transform>();
-        //FL = GameObject.FindGameObjectWithTag("FR").GetComponent<Transform>();
-        //RR = GameObject.FindGameObjectWithTag("RL").GetComponent<Transform>();
-        //RL = GameObject.FindGameObjectWithTag("RR").GetComponent<Transform>();
+
+        FR = GameObject.FindGameObjectWithTag("FL").GetComponent<Transform>();
+        FL = GameObject.FindGameObjectWithTag("FR").GetComponent<Transform>();
+        RR = GameObject.FindGameObjectWithTag("RL").GetComponent<Transform>();
+        RL = GameObject.FindGameObjectWithTag("RR").GetComponent<Transform>();
 
         wc_FL = GameObject.FindGameObjectWithTag("wcFL").GetComponent<WheelCollider>();
         wc_FR = GameObject.FindGameObjectWithTag("wcFR").GetComponent<WheelCollider>();
         wc_RL = GameObject.FindGameObjectWithTag("wcRL").GetComponent<WheelCollider>();
         wc_RR = GameObject.FindGameObjectWithTag("wcRR").GetComponent<WheelCollider>();
     }
-    
+
     void Update()
     {
         Debug.Log(transmission);
@@ -122,10 +154,6 @@ public class VehicleMovementBehaviour : MonoBehaviour
         var accel = Input.GetAxis("Accelerate");
         var brake = Input.GetAxis("Brake");
         var steer = Input.GetAxis("Steer");
-
-        torque = accel * MaxWheelTorque;
-
-        Vector3 move = new Vector3(0, 0, torque * MovementSpeed);
 
         #region Drive
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -145,30 +173,60 @@ public class VehicleMovementBehaviour : MonoBehaviour
         }
 
         Transmission();
-        
-        if (wc_RR.rpm > MaxWheelRPM || wc_RL.rpm > MaxWheelRPM)
+
+        //LIMIT THE MAX SPEED
+        if (velocity.magnitude > MaxVehicleSpeed)
         {
-            wc_RL.motorTorque = 0.0f;
-            wc_RR.motorTorque = 0.0f;
+            torque = 0.0f;
         }
 
-        if (wc_RR.rpm < MaxWheelRPM && wc_RL.rpm < MaxWheelRPM)
+        if (velocity.magnitude < MaxVehicleSpeed)
         {
-            wc_RL.motorTorque = torque;
-            wc_RR.motorTorque = torque;
+            //ADD TORQUE
+            torque += accel * MaxWheelTorque;
         }
 
-        //ROTATE THE WHEEL COLLIDERS
-        wc_FL.steerAngle = steer * MaxSteerAngle;
-        wc_FR.steerAngle = steer * MaxSteerAngle;
+        //force = new Vector3(0, 0, torque * MovementSpeed);
+        force = new Vector3(0, 0, torque);
 
-        wc_FL.brakeTorque = brake * MaxBrakeForce;
-        wc_FR.brakeTorque = brake * MaxBrakeForce;
-        wc_RL.brakeTorque = brake * MaxBrakeForce;
-        wc_RR.brakeTorque = brake * MaxBrakeForce;
-
-        RRwheelRPM = wc_RR.rpm;
-        RLwheelRPM = wc_RL.rpm;
+        Vector3 acceleration = force * Time.deltaTime;
+        velocity = velocity + (acceleration * Time.deltaTime);
         #endregion
+
+        #region Steer
+        //LIMIT THIS BASED ON VEHICLE SPEED
+
+        chassis.Rotate(new Vector3(0, steer * 0.5f, 0));
+
+        if (steer * MaxSteerAngle >= 45.0f || steer * MaxSteerAngle <= -45.0f)
+        {
+            Vector3 slide = new Vector3(driftForce * steer, 0, 0);
+            chassis.position += velocity + slide;
+        }
+
+        if (steer * MaxVehicleSpeed < 45.0f)
+        {
+            chassis.Translate(velocity);
+        }
+        #endregion
+
+        //#region Visuals
+        //wc_FL.steerAngle = steer * MaxSteerAngle;
+        //wc_FR.steerAngle = steer * MaxSteerAngle;
+
+        //Vector3 flPosition = new Vector3();
+        //Quaternion flRotation = new Quaternion();
+        //wc_FL.GetWorldPose(out flPosition, out flRotation);
+
+        //Vector3 frPosition = new Vector3();
+        //Quaternion frRotation = new Quaternion();
+        //wc_FR.GetWorldPose(out frPosition, out frRotation);
+
+        //FL.rotation = flRotation;
+        //FL.position = flPosition;
+
+        //FR.rotation = frRotation;
+        //FR.position = frPosition;
+        //#endregion
     }
 }
