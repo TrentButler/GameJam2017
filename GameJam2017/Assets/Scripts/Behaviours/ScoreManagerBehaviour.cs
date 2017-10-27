5c2d86b2f2c4386dfc3a5196c06f9d7a5941d014
Trent Butler <s178061@students.aie.edu.au>
2017-10-27 13:03:55 -0500

    score manager behavior, ingame ui

---
+++ GameJam2017/Assets/Scripts/Behaviours/ScoreManagerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManagerBehaviour : MonoBehaviour {
+    
+    //GET THE VEHICLE INFO
+    private GetVehicleInfoBehaviour info;
+    
+    //STORE THE INFO
+    //[HideInInspector]
+    public float RPM;
+    //[HideInInspector]
+    public float MPH;
+    [HideInInspector]
+    public int Score;
+
+    //SEND IT OUT
+    public float GetRPM()
+    {
+        return RPM;
+    }
+
+    public float GetMPH()
+    {
+        return MPH;
+    }
+
+    public int GetScore()
+    {
+        return Score;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        info = GameObject.FindGameObjectWithTag("chassis").GetComponent<GetVehicleInfoBehaviour>();	    	
+	}
+
+    private void FixedUpdate()
+    {
+        RPM = info.RPM;
+        MPH = info.MPH;
+        //CALCULATE THE SCORE HERE
+    }
+}
