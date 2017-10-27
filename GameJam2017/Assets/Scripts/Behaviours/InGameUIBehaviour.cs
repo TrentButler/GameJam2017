7b74086be4e1ecddfcb74c0796fca92bdd7fd3df
dylanmitchell1007 <armadummy@yahoo.com>
2017-10-27 15:05:59 -0500

    10/27/2017 Push UI up script 2.0

--- GameJam2017/Assets/Scripts/Behaviours/InGameUIBehaviour.cs
+++ GameJam2017/Assets/Scripts/Behaviours/InGameUIBehaviour.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
 public class InGameUIBehaviour : MonoBehaviour
 {
+    public Text Info;
     ScoreManagerBehaviour manager;
 	// Use this for initialization
 	void Start ()
     {
         manager = GameObject.FindGameObjectWithTag("manager").GetComponent<ScoreManagerBehaviour>();
+        Info = GameObject.FindGameObjectWithTag("Info").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void FixedUpdate()
+    {
+        var MPH = manager.MPH;
+        var RPM = manager.RPM;
+        var Score = manager.Score;
+        string sMPH = "MPH:" + MPH;
+        string sRPM = "RPM:" + RPM;
+        string sScore = "Score:" + Score;
+
+        Info.text = sMPH + "/n" + sRPM + "/n" + sScore + "/n";
+    }
 }
