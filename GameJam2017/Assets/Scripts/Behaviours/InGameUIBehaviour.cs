cdf5167165d5dcac60445c4819327cd98788a41c
dylanmitchell1007 <armadummy@yahoo.com>
2017-10-27 14:44:19 -0500

    10/27/2017 UI SCRIPT

---
+++ GameJam2017/Assets/Scripts/Behaviours/InGameUIBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class InGameUIBehaviour : MonoBehaviour
+{
+    ScoreManagerBehaviour manager;
+	// Use this for initialization
+	void Start ()
+    {
+        manager = GameObject.FindGameObjectWithTag("manager").GetComponent<ScoreManagerBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
